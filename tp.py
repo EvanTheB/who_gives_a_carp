44fde4ffa3b83ba97ef7f37a7b80b33567acba75
Evan Benn <evanbenn@gmail.com>
2021-05-30 17:45:50 +1000

    Fix bug with too many options
    Now correctly narrow down remaining words

--- tp.py
+++ tp.py
@@ -45,6 +45,11 @@ for quad in quads:
 
 allwords = set.union(*list(quadwords.values()))
 
+# mapping from a quad to the other unused quads
+quadtoquads = {}
+for quad in quads:
+	quadtoquads[quad] = list(itertools.combinations(set(range(12)) - set(quad), 4))
+
 # print(len(allwords)) # 1727
 # for less_one in itertools.combinations(list(quadwords.values()), len(quadwords) - 1):
 # 	# these are all at or slightly under 1727, not many missing words
@@ -61,6 +66,38 @@ def out_tp(tp):
 		print(tps[t])
 	print()
 
+def get_remaining_words(used_words):
+	all_ = set(quadwords)
+	possible = set()
+
+	# list of possible quads for each word
+	used = [
+		[quad for quad, words in quadwords.items() if word in words]
+		for word in used_words
+	]
+	# print(used)
+
+	# for every way to make the words from quads
+	for used_quads in itertools.product(*used):
+		used_tps = set.union(*[set(u) for u in used_quads])
+		if len(used_tps) != len("".join(used_words)):
+			# skip if we have re-used some tp
+			continue
+		# Add this iterations unused quads
+		new_tps = set(range(12)) - used_tps
+		possible = set.union(
+			possible,
+			set(itertools.combinations(new_tps, 4))
+		)
+
+		# if we found them all early abort
+		if len(possible) == len(all_):
+			break
+
+	# print(possible)
+
+	return set.union(*list(words for quad, words in quadwords.items() if quad in possible))
+
 import sys
 # phrases are A B C
 if len(sys.argv) == 1:
@@ -70,34 +107,14 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
 	# print all Bs
 	A = sys.argv[1]
-	if len([True for words in quadwords.values() if A in words]) == 1:
-		# A only in 1 list, print others
-		allwords_but_A = set.union(*list(words for words in quadwords.values() if A not in words))
-		out_wordlist(allwords_but_A)
-	else:
-		# A in 2 lists, print all lists
-		out_wordlist(allwords)
+	words = get_remaining_words([A])
+	out_wordlist(words)
 
 elif len(sys.argv) == 3:
 	A = sys.argv[1]
 	B = sys.argv[2]
-
-	all_ = set(quadwords)
-	As = [quad for quad,words in quadwords.items() if A in words]
-	Bs = [quad for quad,words in quadwords.items() if B in words]
-	Cs = set()
-
-	# for every way to make "A B"
-	for A, B in itertools.product(As, Bs):
-		if A == B:
-			continue
-		# find the unfound Cs
-		Cs = set.union(Cs, all_ - set([A]) - set([B]))
-		if Cs == all_:
-			break
-
-	allwords_C = set.union(*list(words for quad, words in quadwords.items() if quad in Cs))
-	out_wordlist(allwords_C)
+	words = get_remaining_words([A, B])
+	out_wordlist(words)
 
 elif len(sys.argv) == 4:
 	# print how to
